c8ff420d948745062fd2edcaa92cebe5dc3088aa
meriem23 <esra.msallem@gmail.com>
2020-03-04 14:49:59 +0100

    Updates

---
+++ precache-manifest.8e5342cc586453908c28e60b9a13b95c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641291494e737c90e1ae79ef47b9fb13",
+    "url": "/Credit-Card-React/index.html"
+  },
+  {
+    "revision": "f20ea53bf766764ae651",
+    "url": "/Credit-Card-React/static/css/2.4121280a.chunk.css"
+  },
+  {
+    "revision": "f9b64b4fb40a94519ae5",
+    "url": "/Credit-Card-React/static/css/main.70dbbf4e.chunk.css"
+  },
+  {
+    "revision": "f20ea53bf766764ae651",
+    "url": "/Credit-Card-React/static/js/2.13a3721d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Credit-Card-React/static/js/2.13a3721d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b64b4fb40a94519ae5",
+    "url": "/Credit-Card-React/static/js/main.0301b8d7.chunk.js"
+  },
+  {
+    "revision": "30e52e807f021a775600",
+    "url": "/Credit-Card-React/static/js/runtime-main.a9608cd3.js"
+  }
+]);
